3465f499eb99e0d5942880c327b57789269d4448
zmiter_mde <zmiter.mde@gmail.com>
2016-03-10 01:04:53 +0300

    Added test sinus plotting

--- graph.js
+++ graph.js
@@ -3,13 +3,16 @@
  */
 function Graph(id, leftX, rightX, bottomY, topY) {
     var vm = this;
+    vm.plotNet = plotNet;
+    vm.plotAxes = plotAxes;
+    vm.setBorders = setBorders;
+    vm.plot = plot;
+
     vm.canvas = document.getElementById(id);
     vm.width = vm.canvas.width;
     vm.height = vm.canvas.height;
+    vm.setBorders(leftX, rightX, bottomY, topY);
     vm.context = vm.canvas.getContext("2d");
-
-    vm.plotNet = plotNet;
-    vm.plotAxes = plotAxes;
 
     function plotNet() {
         vm.context.beginPath();
@@ -22,7 +25,6 @@ function Graph(id, leftX, rightX, bottomY, topY) {
             vm.context.moveTo(0, i);
             vm.context.lineTo(vm.width, i);
         }
-        vm.context.closePath();
         vm.context.strokeStyle = '#eee';
         vm.context.stroke();
     }
@@ -43,10 +45,47 @@ function Graph(id, leftX, rightX, bottomY, topY) {
         vm.context.moveTo(vm.width - 5, middleY + 5);
         vm.context.lineTo(vm.width, middleY);
 
-        vm.context.closePath();
-
         vm.context.strokeStyle = '#000';
         vm.context.stroke();
+    }
+
+    function setBorders(leftX, rightX, bottomY, topY) {
+        if (leftX && rightX && bottomY && topY) {
+            vm.leftX = leftX;
+            vm.rightX = rightX;
+            vm.bottomY = bottomY;
+            vm.topY = topY;
+        } else {
+            if (!leftX && !rightX && !bottomY && !topY) {
+                var proportion = vm.height / vm.width;
+                vm.leftX = -5;
+                vm.rightX = 5;
+                vm.bottomY = vm.leftX * proportion;
+                vm.topY = vm.rightX * proportion;
+            }
+        }
+    }
+
+    function plot(f) {
+        vm.context.beginPath();
+        var x = vm.leftX, stepX = (vm.rightX - vm.leftX) / 100, y = 0;
+        vm.context.moveTo(x, getYPosition(f(x)));
+        while (x < vm.rightX) {
+            x += stepX;
+            y = f(x);
+            console.log(getXPosition(x) + ' ' + getYPosition(y));
+            vm.context.lineTo(getXPosition(x), getYPosition(y));
+        }
+        vm.context.strokeStyle = 'green';
+        vm.context.stroke();
+    }
+
+    function getYPosition(y) {
+        return vm.height - (y - vm.bottomY) / (vm.topY - vm.bottomY) * vm.height;
+    }
+
+    function getXPosition(x) {
+        return vm.width * (x - vm.leftX) / (vm.rightX - vm.leftX);
     }
 
     vm.plotNet();
